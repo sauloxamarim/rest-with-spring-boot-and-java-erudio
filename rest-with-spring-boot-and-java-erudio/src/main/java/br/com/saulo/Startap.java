8e537552dc3255a14d9e4bcc2d8ef789050a8538
Saulo Lopes <lopes.saulo1@gmail.com>
2024-05-25 01:13:11 -0300

    stt

---
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/saulo/Startap.java
@@ -0,0 +1,13 @@
+package br.com.saulo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startap {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startap.class, args);
+	}
+
+}
---
+++ rest-with-spring-boot-and-java-erudio/src/test/java/br/com/saulo/StartapTests.java
@@ -0,0 +1,13 @@
+package br.com.saulo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartapTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
